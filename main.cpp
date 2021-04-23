37033711efc5bf27eb24013c3a5ca9ca769b91a2
Gileibo <leibovicigil@campus.technion.ac.il>
2021-04-23 21:48:54 +0300

    change4

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!2" << std::endl;
+    int a=0;
+    if(a>0)
+        printf("1");
+    else
+        printf("2");
     return 0;
 }
